5d882a24e2d5adb575ad370237c69033757e1ec6
jed <jed@u1s1.com>
2024-11-30 15:11:17 +0400

    整理代码

--- assets/resources/scripts/playerController.ts
+++ assets/resources/scripts/playerController.ts
@@ -8,7 +8,6 @@ import {
   Vec2,
 } from "cc";
 import { generateGridCentersFromTopLeft } from "./uitls/game";
-
 const { ccclass, property } = _decorator;
 
 @ccclass("playercontroller")
@@ -62,28 +61,6 @@ export class playercontroller extends Component {
     } else {
       console.error('The script "grid" is not found on the prefab root node.');
     }
-    // resources.load("Prefab/gamegrid", Prefab, (err, prefab) => {
-    //   if (err) {
-    //     console.log(err);
-    //     return;
-    //   }
-
-    //   console.log("gamegrid loaded successfully!");
-
-    //   const node = instantiate(prefab);
-    //   this.node.addChild(node);
-    //   node.setPosition(0, 0, 0);
-    //   this.node.name = "gamegrid";
-
-    //   const gridScript = node.getComponent("gamegrid");
-    //   if (gridScript) {
-    //     console.log("gridScript", gridScript);
-    //   } else {
-    //     console.error(
-    //       'The script "grid" is not found on the prefab root node.'
-    //     );
-    //   }
-    // });
   }
 
   getGridItem(type: number = 1, point: Vec2 = new Vec2(0, 0)) {
@@ -106,63 +83,6 @@ export class playercontroller extends Component {
     const gridItemNode = instantiate(prefab);
     gridItemNode.setPosition(point.x + groupWidth / 2 - 20, point.y, 0);
     return gridItemNode;
-  }
-
-  /// 获取指定类型的元素
-  getGameItems(type: number = 1) {
-    return this.node.children;
-  }
-
-  initItem1() {
-    console.log("initItem1");
-
-    resources.load("Prefab/group1", Prefab, (err, prefab) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      console.log("group1 loaded successfully!");
-
-      const node = instantiate(prefab);
-      this.node.addChild(node);
-      node.setPosition(0, 0, 0);
-      this.node.name = "group1";
-    });
-  }
-
-  initItem2() {
-    console.log("initItem2");
-    resources.load("Prefab/group2", Prefab, (err, prefab) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      console.log("group2 loaded successfully!");
-
-      const node = instantiate(prefab);
-      this.node.addChild(node);
-      node.setPosition(0, -100, 0);
-      this.node.name = "group2";
-    });
-  }
-
-  initItem3() {
-    console.log("initItem3");
-    resources.load("Prefab/group3", Prefab, (err, prefab) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      console.log("group3 loaded successfully!");
-
-      const node = instantiate(prefab);
-      this.node.addChild(node);
-      node.setPosition(0, -200, 0);
-      this.node.name = "group3";
-    });
   }
 
   update(deltaTime: number) {
